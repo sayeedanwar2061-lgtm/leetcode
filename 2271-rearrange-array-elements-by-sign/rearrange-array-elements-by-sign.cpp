dd4570721b71926d648fd34428e1e0badea1896d
Md Sayeed Anwar <sayeedanwar2061@gmail.com>
2026-09-26 00:47:33 +0000

    Time: 14 ms (13.55%) | Memory: 145.1 MB (7.05%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+
+        vector<int> pos;
+        vector<int> neg;
+
+        for (int x : nums) {
+            if (x > 0)
+                pos.push_back(x);
+            else
+                neg.push_back(x);
+        }
+
+        vector<int> ans;
+
+        for (int i = 0; i < pos.size(); i++) {
+            ans.push_back(pos[i]);
+            ans.push_back(neg[i]);
+        }
+
+        return ans;
+    }
+};
